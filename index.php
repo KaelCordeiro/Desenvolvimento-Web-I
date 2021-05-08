53f074520283f3bc8e9cc4874cad112f79da1fef
carlo <carlo@DESKTOP-MIQEGRG>
2021-05-08 18:17:12 -0300

    Atividade 1 - Lançamento de Dados

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jogo do Lançamento de Dados</title>
+    </head>
+    <body style="background-color: green">
+        
+        <h1 style="color: white">Lançamento de Dados</h1>
+        <h3 style="color: white">Olá Usuário, vamos jogar o lançamento de dados, clique no botão para jogar:</h3>
+        
+        <button type="submit" value="Jogar" onclick="location.href='Dado.php'">Jogar</button>
+             
+    </body>
+</html>
